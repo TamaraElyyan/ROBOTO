8d42f5b5a51558e016cd6989c31e6287c5a10b63
TamaraElyyan <tamaraelyyan1@gmail.com>
2025-01-22 19:34:46 +0200

    Initial commit

---
+++ project_Roboto_.srcs/constrs_1/new/top_module.xdc
@@ -0,0 +1,2 @@
+create_clock -period 10.000 -name clk -waveform {0.000 5.000} [get_ports clk]
+set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets clk_IBUF]
--- project_Roboto_.srcs/sources_1/new/Light_tracker.sv
+++ project_Roboto_.srcs/sources_1/new/Light_tracker.sv
@@ -4,11 +4,13 @@ module Light_tracker (
     input wire [7:0] light_sensor,
     output reg [1:0] motor_control
 );
+    parameter THRESHOLD = 8'h80;
+    
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             motor_control <= 2'b00; // Stop motors on reset
         end else begin
-            if (light_sensor > 8'h80) begin
+            if (light_sensor > THRESHOLD) begin
                 motor_control <= 2'b01; // Move forward if light is high
             end else begin
                 motor_control <= 2'b10; // Move backward if light is low
